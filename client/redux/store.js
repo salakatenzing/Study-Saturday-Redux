3d8f3fae89f85aa86ba96d76552fd6adda2edbfe
Tenzing Salaka <SALAKATENZING@GMAIL.COM>
2021-07-10 11:08:13 -0400

    setting  up store

--- client/redux/store.js
+++ client/redux/store.js
@@ -4,18 +4,32 @@ import axios from 'axios';
 import thunkMiddleware from 'redux-thunk';
 
 // ACTION TYPES go here:
-
+const GOT_STUDENTS = 'GOT_STUDENTS';
 
 // ACTION CREATORS go here:
+const gotStudents = (students) => ({
+type: GOT_STUDENTS,
+students
+})
 
 
 // THUNK CREATORS go here:
+const fetchStudents = () => async (dispatch) => {
+  const {data} = await axios.get('/api/students');
+  dispatch(gotStudents(data))
+}
 
 
-const initialState = {};
+const initialState = {
+  students: []
+};
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case GOT_STUDENTS:
+      return {
+        students: action.students
+      }
     default:
       return state;
   }
